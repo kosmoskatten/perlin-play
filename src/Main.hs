a66ec70e64bfee27c900b5aabe985571aa378f63
Patrik Sandahl <patrik.sandahl@gmail.com>
2017-08-18 15:17:41 +0200

    Rendering a filled circle.

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,20 @@
 module Main where
 
+import           Codec.Picture
+import           Linear        (V2 (..))
+import           Linear.Metric (distance)
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main =
+  writePng "example.png" $ generateImage (filledCircle 512) 512 512
+
+filledCircle :: Int -> Int -> Int -> PixelRGB8
+filledCircle dimension x y =
+    let mid = fromIntegral dimension / 2 :: Float
+        origo = V2 mid mid
+        dist = distance origo $ V2 (fromIntegral x) (fromIntegral y)
+    in
+        if dist < mid then
+            PixelRGB8 255 0 0
+        else
+            PixelRGB8 0 0 0
