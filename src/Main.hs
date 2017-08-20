5eaa2d0d8a41b9aa09fa584116d6787805f6c41a
Patrik Sandahl <kosmoskatten.software@gmail.com>
2017-08-20 22:31:25 +0200

    Somewhat buggy perlin image generation.

--- src/Main.hs
+++ src/Main.hs
@@ -3,10 +3,21 @@ module Main where
 import           Codec.Picture
 import           Linear        (V2 (..))
 import           Linear.Metric (distance)
+import           Perlin        (Perlin)
+import qualified Perlin
 
 main :: IO ()
 main =
-  writePng "example.png" $ generateImage (filledCircle 512) 512 512
+  --writePng "example.png" $ generateImage (filledCircle 512) 512 512
+  writePng "perlin1.png" $ generateImage (perlinImage Perlin.init 32) 512 512
+
+perlinImage :: Perlin -> Double -> Int -> Int -> PixelRGB8
+perlinImage perlin freq x y =
+    let x' = fromIntegral x / 512
+        y' = fromIntegral y / 512
+        nse = clamp (-1) 1 $ Perlin.noise perlin (x' * freq) (y' * freq)
+        px = toPixel nse
+    in PixelRGB8 px px px
 
 filledCircle :: Int -> Int -> Int -> PixelRGB8
 filledCircle dimension x y =
@@ -18,3 +29,9 @@ filledCircle dimension x y =
             PixelRGB8 255 0 0
         else
             PixelRGB8 0 0 0
+
+toPixel :: Double -> Pixel8
+toPixel val = floor $ (val + 1) * 100
+
+clamp :: Double -> Double -> Double -> Double
+clamp mi ma = max mi . min ma
